412ddec85803322222574689f5bf56455156cc97
Luke Becker <luke.w.becker32@gmail.com>
2020-07-20 14:25:09 -0500

    Added mysql exercise commands for the Tables exercise

---
+++ tables_exercises.sql
@@ -0,0 +1,21 @@
+#mysql training for tables exercise
+
+SHOW databases;
+
+USE employees;
+
+SHOW tables;
+
+describe employees;
+
+# 5 the employees table has: INT, DATE, VARCHAR, ENUM data types
+
+# 6 numeric type columns would be in saralies 
+
+# 7 the departments and titles tables would be good candidates for containing columns with strings
+
+# 8 the employees table would absolutely need a DATE data type in it, for employee birth dates
+
+# 9 Use the tables 'dept_emp' and 'dept_manager' to link those two tables together. 
+
+SHOW CREATE TABLE dept_manager;
