178612f9bbb8fd6689c899ce4f57fd4a466a5f70
Luke Becker <luke.w.becker32@gmail.com>
2020-07-21 14:15:51 -0500

    Made corrections to this file

--- select_exercises.sql
+++ select_exercises.sql
@@ -29,4 +29,4 @@ FROM albums
 WHERE genre = 'Rock';
 
 -- This query will only return any album that is specifically categorized as 'Rock', since the query used the equal sign which means 'exactly equal to Rock'
--- However, if I used the "LIKE" operator, in this way: LIKE '%ROCK%', it would pick up any album with the word rock in any part of the genre categorization
+-- However, if I used the "LIKE" operator, in this way: LIKE '%ROCK%', it would pick up any album with the word rock in any part of the genre catego›
