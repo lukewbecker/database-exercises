238aafd0af7b710359f8332a542b88ae0e965457
Luke Becker <luke.w.becker32@gmail.com>
2020-07-21 09:45:53 -0500

    Changed comment character from # to --

--- select_exercises.sql
+++ select_exercises.sql
@@ -28,5 +28,5 @@ SELECT name
 FROM albums
 WHERE genre = 'Rock';
 
-#This query will only return any album that is specifically categorized as 'Rock', since the query used the equal sign which means 'exactly equal to Rock'
-# However, if I used the "LIKE" operator, in this way: LIKE '%ROCK%', it would pick up any album with the word rock in any part of the genre categorization
+-- This query will only return any album that is specifically categorized as 'Rock', since the query used the equal sign which means 'exactly equal to Rock'
+-- However, if I used the "LIKE" operator, in this way: LIKE '%ROCK%', it would pick up any album with the word rock in any part of the genre categorization
