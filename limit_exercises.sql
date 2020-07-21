454ed5b814d46c26c757bfcc47719901759a4a71
Luke Becker <luke.w.becker32@gmail.com>
2020-07-21 16:07:37 -0500

    Adding LIMIT clause exercises to repo

---
+++ limit_exercises.sql
@@ -0,0 +1,32 @@
+USE employees;
+
+# Create a new SQL script named limit_exercises.sql.
+
+# MySQL provides a way to return only unique results from our queries with the keyword DISTINCT.
+
+SELECT DISTINCT title from titles;
+
+# List the first 10 distinct last name sorted in descending order.
+
+SELECT DISTINCT last_name FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+-- Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql. Update it to find just the first 5 employees. 
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
+
+# Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results.
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45;
+
+# LIMIT and OFFSET can be used to create multiple pages of data. What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
+# An algebraic formula would look like this: OFFSET() / LIMIT () = Page number + 1. However, since in this example the data starts on page 1 not page 0, results 45-50 would end up on page 10, not page 9
