e0a7420dc67b3e3231ee72955d977b8c732c1ec2
Luke Becker <luke.w.becker32@gmail.com>
2020-07-24 14:13:19 -0500

    added to join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -139,7 +139,7 @@ JOIN departments AS d ON d.dept_no = de.dept_no
 JOIN dept_manager AS dm ON dm.dept_no = de.dept_no
 JOIN employees AS ee ON dm.emp_no = ee.emp_no
 WHERE de.to_date > CURDATE() AND dm.to_date > CURDATE()
-ORDER BY de.dept_no;
+ORDER BY d.dept_name;
 
 
 -- 11. Bonus Find the highest paid employee in each department.
